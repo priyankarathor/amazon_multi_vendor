f3a10da0dcc41f7b03f54a04e10d7a7d853c7ac2
priyankarathor <priyankaji856@gmail.com>
2026-06-24 14:37:01 +0530

    product model1

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,131 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
-    {
-        productName: {
-            type: String,
-            required: true
-        },
+{
+//product details
+//get details
+  productName: { type: String, required: true },
+  itemName: String,
+  productType: String,
+  recommendedBrowseNode: String,
+  variations: String,
+  brandName: String,
+  externalProductId: String,
 
-        sku: {
-            type: String,
-            required: true
-        },
+  metadata: String,
+  metaKeywords: [String],
+  searchKeywords: [String],
+
+  description: {
+    productDescription: String,
+    bulletPoints: [String],
+    images: [String],
+    multiImages: [String]
+  },
+
+  productDetails: {
+    targetAudienceKeyword: String,
+    modelNumber: String,
+    manufacturer: String,
+    genericKeyword: String,
+    specialFeatures: [String],
+    material: String,
+    itemTypeName: String,
+    color: String,
+    size: String,
+    occasion: String,
+    partNumber: String,
+    itemShape: String,
+    theme: String,
+    manufacturerContactInfo: String,
+    unitCount: Number,
+    unitCountType: String,
+    manufacturerMinimumAgeMonths: Number,
+    includedComponents: [String],
+    leagueName: String,
+    teamName: String
+  },
+
+  dimensions: {
+    itemDimensions: {
+      length: Number,
+      width: Number,
+      height: Number
+    },
+    packageDimensions: {
+      length: Number,
+      width: Number,
+      height: Number
+    },
+    itemWeight: Number,
+    itemWeightUnit: String,
+    packageWeight: Number
+  },
+
+  batteryInfo: {
+    batteryLifePercentage: Number,
+    functionalCondition: String,
+    cosmeticCondition: String,
+    accessories: String
+  },
+
+  packaging: {
+    packagingType: String,
+    sourceType: String,
+    fulfillmentChannel: String,
+    numberOfPacks: Number,
+    masterPackLayersPerPallet: Number
+  },
+
+  safetyCompliance: {
+    countryRegionOfOrigin: String,
+    dangerousGoodsRegulation: String,
+    buyerAgeRestriction: String,
+    mandatoryCautionaryStatement: String,
+    regulatoryComplianceCertification: String,
+    complianceMedia: [String],
+    safetyAttestation: String,
+    safetyAttestationAddress: String,
+    shipsGlobally: Boolean,
+    nonLithiumBatteryEnergyContent: String,
+    lessThan30PercentSOC: Boolean
+  },
+
+  externalInfo: {
+    externalProductInfo: String,
+    externalProductInfoEntity: String,
+    importerContactInformation: String,
+    packerContactInformation: String
+  },
+
+  offer: {
+    sku: String,
+    quantity: Number,
+    handlingTime: Number,
+    restockDate: Date,
+
+    yourPrice: Number,
+    maximumRetailPrice: Number,
+    automatedPricing: Boolean,
+
+    minimumSellerAllowedPrice: Number,
+    maximumSellerAllowedPrice: Number,
+
+    salePrice: Number,
+    saleStartDate: Date,
+    saleEndDate: Date,
+
+    offeringReleaseDate: Date,
+    itemCondition: String,
+    productTaxCode: String,
+    merchantReleaseDate: Date,
+    maximumOrderQuantity: Number
+  },
+
+  giftOptions: {
+    giftMessageAvailable: Boolean,
+    giftWrapAvailable: Boolean
+  },
 
         categoryId: {
             type: mongoose.Schema.Types.ObjectId,
