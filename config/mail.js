c2b2cfa42192f3aed470bf4f63a248ad81d7b4b5
priyankarathor <priyankaji856@gmail.com>
2026-07-02 15:16:52 +0530

    mail

--- config/mail.js
+++ config/mail.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 const dns = require("dns");
 
-dns.setDefaultResultOrder("ipv4first");
+// Force IPv4 resolution explicitly - Render has no outbound IPv6 route to Gmail
+const ipv4Lookup = (hostname, options, callback) => {
+  return dns.lookup(hostname, { family: 4 }, callback);
+};
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
@@ -14,6 +17,10 @@ const transporter = nodemailer.createTransport({
   tls: {
     rejectUnauthorized: false,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  lookup: ipv4Lookup, // forces the actual socket connection to use IPv4
 });
 
 transporter.verify((error, success) => {
