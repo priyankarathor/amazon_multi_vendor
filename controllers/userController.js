e4b347ca7fb525e8738bffa7a4eac4bf5ef00d9b
priyankarathor <priyankaji856@gmail.com>
2026-07-03 15:33:23 +0530

    vender

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const transporter = require("../config/mail");
-const sendWhatsapp = require("../utilS/sendWhatsapp");
+const sendWhatsapp = require("../utils/sendWhatsapp");
 const { generateOtp, getOtpExpiry } = require("../utils/otp");
 
 const PHONE_REGEX = /^\d{10}$/;
