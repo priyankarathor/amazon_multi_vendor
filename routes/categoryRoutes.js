6b1a765922ea232aa0c43a902ee99493ace2a118
priyankarathor <priyankaji856@gmail.com>
2026-07-07 11:50:13 +0530

    delete cate

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -7,6 +7,6 @@ router.post("/add", controller.createCategory);
 router.get("/", controller.getCategories);
 router.get("/:id", controller.getCategory);
 router.put("/:id", controller.updateCategory);
-router.delete("/:id", controller.deleteCategory);
+router.delete("/delete/:id", controller.deleteCategory);
 
 module.exports = router;
