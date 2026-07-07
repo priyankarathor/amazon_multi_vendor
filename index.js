14020e72476c6a14fafc109ec7b2908ef21ee16e
priyankarathor <priyankaji856@gmail.com>
2026-07-07 14:26:04 +0530

    details

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/api/banners', require('./routes/bannerRoutes'));
 app.use('/api/admin/banners', require('./routes/admin/bannerRoutes'));
 app.use("/api/cart", require("./routes/cartRoutes"));
 app.use("/api/wishrlist", require("./routes/wishlistRoutes"));
-app.use("/api/ordes",require("./routes/orderRoutes"));
+app.use("/api/orders",require("./routes/orderRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
